3e7220b84b352f80f922ab1ce99cafda90faa964
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-07-25 16:28:07 -0300

    --

---
+++ script.js
@@ -0,0 +1,22 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.querySelector('#liters')
+const percentage = document.querySelector('.percentage')
+const remained = document.querySelector('.remained')
+
+smallCups.forEach((cup, idx)=> {
+  cup.addEventListener('click', () => hightLightCups(idx))
+});
+
+function hightLightCups(idx) {  
+  if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+    idx--
+  }
+  
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add('full')
+    } else {
+      cup.classList.remove('full')
+    }
+  })
+}
